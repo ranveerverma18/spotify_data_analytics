fb60ea1f707197cc920df879f628b8243301fc63
Ranveer Verma <ranveerverma2003@gmail.com>
2025-04-14 13:18:54 +0530

    API key initialization

--- producer_script.py
+++ producer_script.py
@@ -9,5 +9,14 @@ from confluent_kafka.serialization import StringSerializer
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+kafka_config = {
+    'bootstrap.servers': 'pkc-l7pr2.ap-south-1.aws.confluent.cloud:9092', 
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': 'YBZYWVVR4ZHOM7LJ',
+    'sasl.password': '8AZFKxD8XSgYhYpcg25Z4yQEIVUKVLPa0+SAkW9LjNwWVVK9RJZguxluzadIbFcr'
+}
+
+
 
 
