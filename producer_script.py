5e0d3b47efcea0deb800ac28c95bec64fdef6c43
Ranveer Verma <ranveerverma2003@gmail.com>
2025-04-14 13:13:38 +0530

    started with the producer script for kafka imported all the necessary libraries

---
+++ producer_script.py
@@ -0,0 +1,13 @@
+import time
+import random
+import uuid
+from datetime import datetime
+from confluent_kafka import SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import StringSerializer
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+
+
